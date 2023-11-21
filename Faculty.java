4fed13ab18b7b88fcd5edfb58631932e6a4fb51e
fosullyy <149721122+fosullyy@users.noreply.github.com>
2023-11-21 16:01:52 +0000

    Added viewTranscript and submitResults methods

--- Faculty.java
+++ Faculty.java
@@ -2,14 +2,12 @@ import java.util.ArrayList;
 
 public class Faculty {
     private String name;
-    private ArrayList<Student> students;
     private ArrayList<Module> modules;
     private ArrayList<Programme> programmes;
     private Department department;
 
-    public Faculty(String name, ArrayList<Student> students, ArrayList<Module> modules, ArrayList<Programme> programmes, Department department) {
+    public Faculty(String name, ArrayList<Module> modules, ArrayList<Programme> programmes, Department department) {
         this.name = name;
-        this.students = students;
         this.modules = modules;
         this.programmes = programmes;
         this.department = department;
@@ -21,14 +19,6 @@ public class Faculty {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public ArrayList<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(ArrayList<Student> students) {
-        this.students = students;
     }
 
     public ArrayList<Module> getModules() {
@@ -54,4 +44,15 @@ public class Faculty {
     public void setDepartment(Department department) {
         this.department = department;
     }
+
+    public void submitResults(Student student, ArrayList<Result> results) {
+        for (Result result : results) {
+            student.addResult(result);
+        }
+    }
+
+    public Transcript viewTranscript(Student student) {
+        return new Transcript(student);
+    }
 }
+
