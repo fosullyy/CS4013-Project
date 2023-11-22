ed6b9993cbcc63bf64cb5c2f0a69dfc42e879cf8
patrickkearney2 <79634448+patrickkearney2@users.noreply.github.com>
2023-11-22 21:06:37 +0000

    Update Transcript.java
    ranger nelligan

--- Transcript.java
+++ Transcript.java
@@ -12,4 +12,27 @@ public class Transcript {
     public void setStudent(Student student) {
         this.student = student;
     }
+    public class Test {
+    public static void main(String[] args) {
+        int borderWidth = 80;
+        int borderHeight = 10;
+
+     
+        System.out.println("+" + "-".repeat(borderWidth - 2) + "+");
+        
+        for (int i = 0; i < borderHeight; i++) {
+         
+            if (i == verticalLinePosition) {
+              
+                System.out.println("|" + " ".repeat(borderWidth - 4) + "|" + " ".repeat(1));
+            } else {
+              
+                System.out.println("|" + " ".repeat(borderWidth - 2) + "|");
+            }
+        }
+
+        System.out.println("+" + "-".repeat(borderWidth - 2) + "+");
+    }
+}
+
 }
