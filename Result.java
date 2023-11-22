65fa8632ccb9f76b63f30ab1097490222b2d1506
fosullyy <149721122+fosullyy@users.noreply.github.com>
2023-11-22 23:24:30 +0000

    Added semester variable

--- Result.java
+++ Result.java
@@ -3,11 +3,14 @@ import java.util.ArrayList;
 public class Result {
     private Module module;
     private String grade;
+    private int semester;
 
-    public Result(Module module, String grade) {
+    public Result(Module module, String grade, int semester) {
         this.module = module;
         this.grade = grade;
+        this.semester = semester;
     }
+
     public Module getModule() {
         return module;
     }
@@ -23,5 +26,12 @@ public class Result {
     public void setGrade(String grade) {
         this.grade = grade;
     }
-}
 
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+}
