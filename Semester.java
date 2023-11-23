5570282fd88290e614754e7cd3fa032f888767e2
fosullyy <149721122+fosullyy@users.noreply.github.com>
2023-11-23 18:43:35 +0000

    Update Semester.java

--- Semester.java
+++ Semester.java
@@ -1,8 +1,17 @@
-public class Semester extends Year {
+public class Semester {
     private int semNumber;
+    private int yearNumber;
     public Semester(int yearNumber, int semNumber) {
-        super(yearNumber);
+        this.yearNumber = yearNumber;
         this.semNumber = semNumber;
+    }
+
+    public int getYearNumber() {
+        return yearNumber;
+    }
+
+    public void setYearNumber(int yearNumber) {
+        this.yearNumber = yearNumber;
     }
 
     public int getSemNumber() {
