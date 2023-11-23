3e6a38ee05f5a5b46a55b223afe796889f789086
fosullyy <149721122+fosullyy@users.noreply.github.com>
2023-11-23 18:45:29 +0000

    Update Programme.java

--- Programme.java
+++ Programme.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
 
-public class Programme extends Department {
+public class Programme {
     private String programmeName;
     private String programmeId;
+    private Department department;
     private ArrayList<Module> modules;
+    private ArrayList<Student> students;
 
-    public Programme(String departmentName, ArrayList<Programme> programmes,
-                     String programmeName, String programmeId, ArrayList<Module> modules) {
-        super(departmentName, programmes);
+    public Programme(String programmeName, String programmeId, Department department,
+                     ArrayList<Module> modules, ArrayList<Student> students) {
         this.programmeName = programmeName;
         this.programmeId = programmeId;
+        this.department = department;
         this.modules = modules;
+        this.students = students;
     }
 
     public String getProgrammeName() {
@@ -29,6 +32,14 @@ public class Programme extends Department {
         this.programmeId = programmeId;
     }
 
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
     public ArrayList<Module> getModules() {
         return modules;
     }
@@ -36,5 +47,12 @@ public class Programme extends Department {
     public void setModules(ArrayList<Module> modules) {
         this.modules = modules;
     }
-}
 
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+}
