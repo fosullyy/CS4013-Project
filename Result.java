06d4623fff764ca92c76c52753eea9178ffe5ae9
fosullyy <149721122+fosullyy@users.noreply.github.com>
2023-11-23 15:23:56 +0000

    Update Result.java

--- Result.java
+++ Result.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class Result {
     private Module module;
     private String grade;
-    private int semester;
+    private Semester semester;
 
-    public Result(Module module, String grade, int semester) {
+    public Result(Module module, String grade, Semester semester) {
         this.module = module;
         this.grade = grade;
         this.semester = semester;
@@ -27,11 +27,64 @@ public class Result {
         this.grade = grade;
     }
 
-    public int getSemester() {
+    public Semester getSemester() {
         return semester;
     }
 
-    public void setSemester(int semester) {
+    public void setSemester(Semester semester) {
         this.semester = semester;
+    }
+
+    public double calculateQCA(ArrayList<Result> results) {
+        ArrayList<String> grades = new ArrayList<>();
+
+        for (Result result : results) {
+            grades.add(result.getGrade());
+        }
+
+        double total = 0.0;
+        int moduleNumber = grades.size();
+
+        for (String grade : grades) {
+            switch (grade.toUpperCase()) {
+                case "A1":
+                    total += 4.0;
+                    break;
+                case "A2":
+                    total += 3.6;
+                    break;
+                case "B1":
+                    total += 3.2;
+                    break;
+                case "B2":
+                    total += 3.0;
+                    break;
+                case "B3":
+                    total += 2.8;
+                    break;
+                case "C1":
+                    total += 2.6;
+                    break;
+                case "C2":
+                    total += 2.4;
+                    break;
+                case "C3":
+                    total += 2.0;
+                    break;
+                case "D1":
+                    total += 1.6;
+                    break;
+                case "D2":
+                    total += 1.2;
+                    break;
+                case "F", "NG":
+                    total += 0.0;
+                    break;
+            }
+        }
+
+        double qca = total / moduleNumber;
+        String roundedQca = String.format("%.2f", qca);
+        return Double.parseDouble(roundedQca);
     }
 }
