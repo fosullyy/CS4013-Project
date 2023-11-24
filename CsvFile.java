c7a2c0a950449a4919b2c8806108ab3f663acffd
fosullyy <149721122+fosullyy@users.noreply.github.com>
2023-11-24 12:06:07 +0000

    Update CsvFile.java

--- CsvFile.java
+++ CsvFile.java
@@ -85,5 +85,31 @@ public class CsvFile {
             throw new RuntimeException(e);
         }
     }
+
+    public ArrayList<Module> readModules(String filepath) {
+        ArrayList<Module> bookOfModules = new ArrayList<>();
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            while ((line = reader.readLine()) != null) {
+                String[] moduleData = line.split(",");
+
+                if (moduleData.length == 3) {
+                    String moduleId = moduleData[0].trim();
+                    String moduleName = moduleData[1].trim();
+                    int credits = Integer.parseInt(moduleData[2].trim());
+
+                    Module module = new Module(moduleName, moduleId, credits);
+                    bookOfModules.add(module);
+                } else {
+                    System.out.print("No module found on line " + line);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return bookOfModules;
+    }
 }
 
